e2c9deb60940932ed51567b7e68d73397d207f7a
cat2151 <fct_2004@mail.goo.ne.jp>
2022-04-10 12:55:25 +0900

    Releasesで配布するようにしたのにあわせ、それに向いたファイルを生成できるようにした

--- build_and_test/build.bat
+++ build_and_test/build.bat
@@ -3,26 +3,26 @@
 function build() {
     copy ..\src\miniIncrementalSearchFilter.py .
     python setup.py py2exe
-    xcopy /E /I /Y miniIncrementalSearchFilter\*.* ..
-    Remove-Item miniIncrementalSearchFilter -Recurse
+#    xcopy /E /I /Y miniIncrementalSearchFilter\*.* ..
+#    Remove-Item miniIncrementalSearchFilter -Recurse
     del miniIncrementalSearchFilter.py
 }
 
 function install_migemo() {
-    pushd ..
+    pushd miniIncrementalSearchFilter
     curl.exe -L https://raw.githubusercontent.com/cat2151/migemo-auto-install-for-windows/main/install_cmigemo.bat --output install_cmigemo.bat
     cmd /c install_cmigemo.bat
     popd
 }
 
 function test() {
-    pushd ..
-    cmd /c miniIncrementalSearchFilter.exe src\miniIncrementalSearchFilter.py output.txt
+    pushd miniIncrementalSearchFilter
+    cmd /c miniIncrementalSearchFilter.exe ..\..\src\miniIncrementalSearchFilter.py output.txt
     popd
 }
 
 function remove() {
-    pushd ..
+    pushd miniIncrementalSearchFilter
     Remove-Item dict -Recurse
     del migemo.dll
     del cmigemo-default-win64-20110227.zip
--- build_and_test/create_release_binary.bat
+++ build_and_test/create_release_binary.bat
@@ -1,14 +1,7 @@
 @powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
 
 function copy_files() {
-    mkdir miniIncrementalSearchFilter
-    xcopy /E /I /Y ..\lib\*.*               miniIncrementalSearchFilter\lib
-    copy ..\libcrypto-1_1.dll               miniIncrementalSearchFilter
-    copy ..\libffi-7.dll                    miniIncrementalSearchFilter
-    copy ..\tcl86t.dll                      miniIncrementalSearchFilter
-    copy ..\tk86t.dll                       miniIncrementalSearchFilter
-    copy ..\miniIncrementalSearchFilter.exe miniIncrementalSearchFilter
-    copy ..\README.md                       miniIncrementalSearchFilter
+    copy ..\README.md miniIncrementalSearchFilter
 }
 
 function compress() {
